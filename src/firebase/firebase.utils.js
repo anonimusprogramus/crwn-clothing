f6a8bcaf696805320fbf63863862ff9245e962f4
Anonimus Programus <anonimus.programus@gmail.com>
2019-12-30 00:33:24 +0700

    fixed exposed google api key in firebase.utils.js, accidentaly comitted to github, now in own file, git-ignored

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,15 +2,7 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
 
-const config = {
-  apiKey: 'AIzaSyBoMilRCgk8sAo6hv3ch1hC-dFdZUdkGWQ',
-  authDomain: 'crwn-clothing-db-kc.firebaseapp.com',
-  databaseURL: 'https://crwn-clothing-db-kc.firebaseio.com',
-  projectId: 'crwn-clothing-db-kc',
-  storageBucket: 'crwn-clothing-db-kc.appspot.com',
-  messagingSenderId: '611095468051',
-  appId: '1:611095468051:web:2a12afe3e6865135e287e6'
-}
+import config from './firebase.config'
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return
